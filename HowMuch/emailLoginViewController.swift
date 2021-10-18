098def4469ec5f772272f09a78829b7bb36dd09b
sobamemil <sieh96@naver.com>
2021-10-18 23:40:16 +0900

    emailLoginViewController에서 emailLoginClicked 메소드 추가. 로그인 버튼 클릭 시 alamofire를 통한 get 요청

--- HowMuch/emailLoginViewController.swift
+++ HowMuch/emailLoginViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Alamofire
+import SwiftUI
 
 class emailLoginViewController : UIViewController {
     @IBOutlet weak var tfEmail: UITextField!
@@ -24,5 +25,47 @@ class emailLoginViewController : UIViewController {
     }
     
     
+    @IBAction func emailLoginClicked(_ sender: UIButton) {
+        let url = "https://jsonplaceholder.typicode.com/todos/1"
+                AF.request(url,
+                           method: .get,
+                           parameters: nil,
+                           encoding: URLEncoding.default,
+                           headers: ["Content-Type":"application/json", "Accept":"application/json"])
+                    .validate(statusCode: 200..<300)
+                    .responseJSON { (json) in
+                        //여기서 가져온 데이터를 자유롭게 활용하세요.
+                        print(json)
+                }
+    }
+    
+    
 }
+
+// 사용자 로그인 계정 정보
+final class UserModel {
+    struct User {
+        var email: String
+        var password: String
+    }
+    
+    var users: [User] = [
+        User(email: "abc1234@naver.com", password: "qwerty1234"),
+        User(email: "dazzlynnnn@gmail.com", password: "asdfasdf5678")
+    ]
+    
+    // 아이디 형식 검사
+    func isValidEmail(id: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: id)
+    }
+    
+    // 비밀번호 형식 검사
+    func isValidPassword(pwd: String) -> Bool {
+        let passwordRegEx = "^[a-zA-Z0-9]{8,}$"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+        return passwordTest.evaluate(with: pwd)
+    }
+} // end of UserModel
 
