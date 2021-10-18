c7166c57189c91b9c71e86526758a1baad0c4758
sobamemil <sieh96@naver.com>
2021-10-19 00:12:19 +0900

    정규식 등을 통한 로그인 형식 체크 및 로그인 기능 구현. mainviewcontroller 생성. 로그인 성공 시 mainviewcontroller로 넘어감.

---
+++ HowMuch/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  HowMuch
+//
+//  Created by 심찬영 on 2021/10/19.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
--- HowMuch/emailLoginViewController.swift
+++ HowMuch/emailLoginViewController.swift
@@ -10,6 +10,9 @@ import Alamofire
 import SwiftUI
 
 class emailLoginViewController : UIViewController {
+    
+    var userModel = UserModel() // 사용자 계정 인스턴스 생성
+
     @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var tfPwd: UITextField!
     
@@ -19,6 +22,32 @@ class emailLoginViewController : UIViewController {
             tfPwd.becomeFirstResponder()
         }
     }
+    
+    // TextField 흔들기 애니메이션
+    func shakeTextField(textField: UITextField) -> Void{
+        UIView.animate(withDuration: 0.2, animations: {
+            textField.frame.origin.x -= 10
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.2, animations: {
+                textField.frame.origin.x += 20
+             }, completion: { _ in
+                 UIView.animate(withDuration: 0.2, animations: {
+                    textField.frame.origin.x -= 10
+                })
+            })
+        })
+    }
+    
+    // 로그인 method
+    func loginCheck(id: String, pwd: String) -> Bool {
+        for user in userModel.users {
+            if user.email == id && user.password == pwd {
+                return true // 로그인 성공
+            }
+        }
+        return false
+    }
+       
     
     override func viewDidLoad() {
         // 다크모드 미적용.
@@ -34,8 +63,66 @@ class emailLoginViewController : UIViewController {
         
     }
     
-    
+    // "이메일로 로그인" 버튼 클릭
     @IBAction func emailLoginClicked(_ sender: UIButton) {
+        // 옵셔널 바인딩 & 예외 처리 : Textfield가 빈문자열이 아니고, nil이 아닐 때
+            guard let email = tfEmail.text, !email.isEmpty else { return }
+            guard let password = tfPwd.text, !password.isEmpty else { return }
+        
+        if userModel.isValidEmail(id: email){
+                if let removable = self.view.viewWithTag(100) {
+                    removable.removeFromSuperview()
+                }
+            }
+            else {
+                shakeTextField(textField: tfEmail)
+                let emailLabel = UILabel(frame: CGRect(x: 68, y: 350, width: 279, height: 45))
+                emailLabel.text = "이메일 형식을 확인해 주세요"
+                emailLabel.textColor = UIColor.red
+                emailLabel.tag = 100
+                    
+                self.view.addSubview(emailLabel)
+            } // 이메일 형식 오류
+        
+        if userModel.isValidPassword(pwd: password){
+                if let removable = self.view.viewWithTag(101) {
+                    removable.removeFromSuperview()
+                }
+            }
+            else{
+                shakeTextField(textField: tfPwd)
+                let passwordLabel = UILabel(frame: CGRect(x: 68, y: 435, width: 279, height: 45))
+                passwordLabel.text = "비밀번호 형식을 확인해 주세요"
+                passwordLabel.textColor = UIColor.red
+                passwordLabel.tag = 101
+                    
+                self.view.addSubview(passwordLabel)
+            } // 비밀번호 형식 오류
+        
+        if userModel.isValidEmail(id: email) && userModel.isValidPassword(pwd: password) {
+                let loginSuccess: Bool = loginCheck(id: email, pwd: password)
+                if loginSuccess {
+                    print("로그인 성공")
+                    if let removable = self.view.viewWithTag(102) {
+                        removable.removeFromSuperview()
+                    }
+                    self.performSegue(withIdentifier: "showMain", sender: self)
+                }
+                else {
+                    print("로그인 실패")
+                    shakeTextField(textField: tfEmail)
+                    shakeTextField(textField: tfPwd)
+                    let loginFailLabel = UILabel(frame: CGRect(x: 68, y: 510, width: 279, height: 45))
+                    loginFailLabel.text = "아이디나 비밀번호가 다릅니다."
+                    loginFailLabel.textColor = UIColor.red
+                    loginFailLabel.tag = 102
+                        
+                    self.view.addSubview(loginFailLabel)
+                }
+            }
+        
+        
+        
         let url = "https://jsonplaceholder.typicode.com/todos/1"
                 AF.request(url,
                            method: .get,
@@ -61,7 +148,7 @@ final class UserModel {
     
     var users: [User] = [
         User(email: "abc1234@naver.com", password: "qwerty1234"),
-        User(email: "dazzlynnnn@gmail.com", password: "asdfasdf5678")
+        User(email: "test@naver.com", password: "12341234")
     ]
     
     // 아이디 형식 검사
