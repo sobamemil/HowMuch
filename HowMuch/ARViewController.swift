f7bf2dc4622e9f4fcd42f694d79ccdf5c2f9c77e
sobamemil <sieh96@naver.com>
2021-11-01 14:05:18 +0900

    폐기물 길이 측정을 위한 ARSCNView 추가, ARViewController 추가

---
+++ HowMuch/ARViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ARViewController.swift
+//  HowMuch
+//
+//  Created by 심찬영 on 2021/11/01.
+//
+
+import UIKit
+import ARKit
+
+class ARViewController : UIViewController, ARSCNViewDelegate {
+    
+    override func viewDidLoad() {
+        
+    }
+}
